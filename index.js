88efeab8dbcdc086acd12bd2d01fd3b8ac221a81
Ravinshu Makkar <mravinshu@gmail.com>
2022-06-20 16:38:43 +0530

    aalu

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("WORKING!!!");
+  res.send("Welcome Aalu");
 });
 
 app.get("/aalu", function (req, res) {

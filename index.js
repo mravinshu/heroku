947de77a4d67b130f0f38369a5768f11335db1fc
Ravinshu Makkar <mravinshu@gmail.com>
2022-06-20 16:34:29 +0530

    aalu

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get("/", function (req, res) {
   res.send("WORKING!!!");
 });
 
+app.get("/aalu", function (req, res) {
+    res.send("Aalu is a good dog");
+})
+
 app.listen(process.env.PORT || 5000);
